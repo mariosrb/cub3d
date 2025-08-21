0bf858179386a10145fd3bc643e8044d4b379a7e
Mario Dodevski <mdodevsk@f2r5s7.clusters.42paris.fr>
2025-08-21 11:39:25 +0200

    Camera test

--- includes/game.h
+++ includes/game.h
@@ -17,7 +17,7 @@
 # define D 100
 # define LEFT 65361
 # define RIGHT 65363
-# define DEBUG 1
+# define DEBUG 0
 
 # define PI 3.14159265359
 
@@ -33,6 +33,11 @@ typedef struct s_player
 	float	pos_y;
 	float	angle;
 
+	double	dirX;
+	double	dirY;
+	double	planeX;
+	double	planeY;
+	
 	bool	move_up;
 	bool	move_down;
 	bool	move_left;
