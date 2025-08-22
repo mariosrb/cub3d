b5a433e59e20bdff1d7d6432acb07b6e175099ab
Mario Dodevski <mdodevsk@f2r5s22.clusters.42paris.fr>
2025-08-22 13:21:56 +0200

    norme movement

--- src/input/movement.c
+++ src/input/movement.c
@@ -1,52 +1,54 @@
 #include "../../includes/cub3d.h"
 
+static void rotate_left(t_player *p, double angle_speed)
+{
+	double	old_dir_x;
+	double	old_plane_x;
+
+	old_dir_x = p->dirX;
+	p->dirX = p->dirX * cos(angle_speed) - p->dirY * sin(angle_speed);
+	p->dirY = old_dir_x * sin(angle_speed) + p->dirY * cos(angle_speed);
+	old_plane_x = p->planeX;
+	p->planeX = p->planeX * cos(angle_speed) - p->planeY * sin(angle_speed);
+	p->planeY = old_plane_x * sin(angle_speed) + p->planeY * cos(angle_speed);
+}
+
+static void rotate_right(t_player *p, double angle_speed)
+{
+	double	old_dir_x;
+	double	old_plane_x;
+
+	old_dir_x = p->dirX;
+	p->dirX = p->dirX * cos(-angle_speed) - p->dirY * sin(-angle_speed);
+	p->dirY = old_dir_x * sin(-angle_speed) + p->dirY * cos(-angle_speed);
+	old_plane_x = p->planeX;
+	p->planeX = p->planeX * cos(-angle_speed) - p->planeY * sin(-angle_speed);
+	p->planeY = old_plane_x * sin(-angle_speed) + p->planeY * cos(-angle_speed);
+}
+
+static void	move_player(t_player *p, double vec_x, double vec_y, double speed)
+{
+	p->pos_x += vec_x * speed;
+	p->pos_y += vec_y * speed;
+}
+
 void player_move(t_player *player)
 {
-    double speed;
-    double angle_speed;
+	double	speed;
+	double	angle_speed;
 
 	speed = 3.0;
 	angle_speed = 0.01;
-    if (player->rotate_left)
-    {
-        double oldDirX = player->dirX;
-        player->dirX = player->dirX * cos(angle_speed) - player->dirY * sin(angle_speed);
-        player->dirY = oldDirX * sin(angle_speed) + player->dirY * cos(angle_speed);
-
-        double oldPlaneX = player->planeX;
-        player->planeX = player->planeX * cos(angle_speed) - player->planeY * sin(angle_speed);
-        player->planeY = oldPlaneX * sin(angle_speed) + player->planeY * cos(angle_speed);
-    }
-
-    if (player->rotate_right)
-    {
-        double oldDirX = player->dirX;
-        player->dirX = player->dirX * cos(-angle_speed) - player->dirY * sin(-angle_speed);
-        player->dirY = oldDirX * sin(-angle_speed) + player->dirY * cos(-angle_speed);
-
-        double oldPlaneX = player->planeX;
-        player->planeX = player->planeX * cos(-angle_speed) - player->planeY * sin(-angle_speed);
-        player->planeY = oldPlaneX * sin(-angle_speed) + player->planeY * cos(-angle_speed);
-    }
-
+	if (player->rotate_left)
+		rotate_left(player, angle_speed);
+	if (player->rotate_right)
+		rotate_right(player, angle_speed);
 	if (player->move_up)
-    {
-        player->pos_x += player->dirX * speed;
-        player->pos_y += player->dirY * speed;
-    }
-    if (player->move_down)
-    {
-        player->pos_x -= player->dirX * speed;
-        player->pos_y -= player->dirY * speed;
-    }
-    if (player->move_left)
-    {
-        player->pos_x -= player->planeX * speed;
-        player->pos_y -= player->planeY * speed;
-    }
-    if (player->move_right)
-    {
-        player->pos_x += player->planeX * speed;
-        player->pos_y += player->planeY * speed;
-    }
+		move_player(player, player->dirX, player->dirY, speed);
+	if (player->move_down)
+		move_player(player, -player->dirX, -player->dirY, speed);
+	if (player->move_left)
+		move_player(player, -player->planeX, -player->planeY, speed);
+	if (player->move_right)
+		move_player(player, player->planeX, player->planeY, speed);
 }
